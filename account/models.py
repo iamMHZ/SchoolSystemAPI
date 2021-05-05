91db1e12f2171ef3aa75ed5b51be5435a0ba0d6c
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-05-05 22:39:39 +0430

    added custom user model #3

--- account/models.py
+++ account/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_teacher(self):
+        pass
+
+    def create_student(self):
+        pass
+
+    def create_superuser(self):
+        pass
+
+
+class User(AbstractUser):
+    school_name = models.CharField(max_length=255, null=True)
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+
+    is_active = False
+    is_staff = False
+    is_superuser = False
+
+    # USERNAME_FIELD = 'national id'
+    # REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
