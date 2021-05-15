a0060b0521f4a2651bc9ab8a0a03b9c5ed736449
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-05-16 00:12:40 +0430

    changes urls  #2

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from .views import UserCreateView, UserListView
 
 urlpatterns = [
 
-    path('create-user', UserCreateView.as_view()),
-    path('list-user', UserListView.as_view()),
+    path('create-account', UserCreateView.as_view()),
+    path('list-account', UserListView.as_view()),
 
 ]
