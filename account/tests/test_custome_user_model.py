75ab12d4a3ea51654336d269cdbde8301c0c45ec
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-05-15 23:18:23 +0430

    tests for creating a simple user #1

--- account/tests/test_custome_user_model.py
+++ account/tests/test_custome_user_model.py
@@ -1,5 +1,25 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 
 class CustomUserTests(TestCase):
-    pass
+
+    def test_create_user_success(self):
+        """Test creating a simple active user is successful"""
+
+        username = 'test'
+        password = '12345'
+
+        user = get_user_model().objects.create_user(
+            username=username,
+            password=password,
+
+        )
+
+        self.assertEqual(user.username, username)
+        self.assertTrue(user.check_password(password))
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_teacher)
+        self.assertFalse(user.is_student)
