0df4af54cb784d088b792c5a248f10edf3554c56
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-05-15 22:01:26 +0430

    Customized admin page #6

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,44 @@
-from django.contrib import admin
-
 # Register your models here.
 
-# TODO add custom user admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import gettext
+
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    """Customize the admin page """
+    # https://docs.djangoproject.com/en/3.2/topics/auth/customizing/
+
+    ordering = ['id']
+    list_display = ['username', 'school_name']
+
+    # use for editing a user
+    fieldsets = (
+        (None, {'fields': ('username', 'password',)}),
+        (gettext('Personal Info'), {'fields': ('first_name', 'last_name', 'school_name',)}),
+        (
+            gettext('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_student', 'is_teacher',)}
+        ),
+        (gettext('Important dates'), {'fields': ('last_login', 'date_joined',)})
+    )
+
+    # 'user for creating user'
+    add_fieldsets = (
+
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username', 'first_name', 'last_name', 'school_name',
+                'password1', 'password2',
+                'is_active', 'is_staff', 'is_superuser',
+                'is_student', 'is_teacher',
+            ),
+        }),
+
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
