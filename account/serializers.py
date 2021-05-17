10b76f1b441ca8fe2e67a9b0b9a7031ed6ad0557
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-05-18 01:05:49 +0430

    Simple user serializer #5

--- account/serializers.py
+++ account/serializers.py
@@ -3,6 +3,16 @@ from rest_framework.serializers import ModelSerializer
 
 
 class UserSerializer(ModelSerializer):
+    """Serializer with full access for all parameters of the custom model"""
+
     class Meta:
         model = get_user_model()
         fields = '__all__'
+
+
+class SimpleUserSerializer(ModelSerializer):
+    """A serializer with for creating a simple user only """
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password',)
