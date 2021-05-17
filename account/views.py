e509dce1b81ca6778efa7dce02473862c4b3de6f
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-05-18 01:06:18 +0430

    Added simple serializer to the view #2

--- account/views.py
+++ account/views.py
@@ -1,11 +1,17 @@
 from django.contrib.auth import get_user_model
 from rest_framework.generics import ListAPIView, CreateAPIView
 
-from .serializers import UserSerializer
+from account.serializers import UserSerializer, SimpleUserSerializer
 
 
 class UserCreateView(CreateAPIView):
     serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.request.user.is_superuser:
+            return self.serializer_class
+
+        return SimpleUserSerializer
 
 
 class UserListView(ListAPIView):
