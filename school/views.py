5d1a1ff246d7acc2e2f1849ddb92afbad8792dfc
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-05-19 14:18:22 +0430

    Create news for the authenticated user only #2

--- school/views.py
+++ school/views.py
@@ -8,8 +8,12 @@ from school import serializers
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = models.News.objects.all()
     serializer_class = serializers.NewsSerializer
-    permission_classes = permissions.IsAuthenticated
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         """limit the news to the authenticated user"""
         return self.queryset.filter(teacher=self.request.user)
+
+    def perform_create(self, serializer):
+        """Save the news for the authenticated user"""
+        serializer.save(teacher=self.request.user)
