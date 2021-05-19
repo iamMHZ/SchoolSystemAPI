c1cf53c7c21debcdff477ef96a1ebf62ca4c5b6e
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-05-19 14:15:07 +0430

    added permission and limited news to the authenticated user  #2

--- school/views.py
+++ school/views.py
@@ -1,3 +1,4 @@
+from rest_framework import permissions
 from rest_framework import viewsets
 
 from school import models
@@ -7,3 +8,8 @@ from school import serializers
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = models.News.objects.all()
     serializer_class = serializers.NewsSerializer
+    permission_classes = permissions.IsAuthenticated
+
+    def get_queryset(self):
+        """limit the news to the authenticated user"""
+        return self.queryset.filter(teacher=self.request.user)
