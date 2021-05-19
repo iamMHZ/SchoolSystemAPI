d1d33707ac77457c1e0be0a46bbfd8e07b0963a1
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-05-19 13:46:56 +0430

    news model

--- school/models.py
+++ school/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    body = models.TextField(max_length=600)
+
+    # students = models.ManyToManyField(to=settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return str(self.title)
+
+# class Assignment(models.Model):
+#     pass
+
+
+# class Task(models.Model):
+#     pass
