458e80af72d5350bfa79e885d2c8a4110e5fae86
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-05-23 23:40:59 +0430

    add student urls #2

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import UserCreateView, UserListView
+from .views import UserCreateView, UserListView,CreateStudentView
 
 app_name = 'account'
 
@@ -8,5 +8,7 @@ urlpatterns = [
 
     path('create-account', UserCreateView.as_view(), name='create-account'),
     path('list-account', UserListView.as_view(), name='list-accounts'),
+
+    path('add-student', CreateStudentView.as_view(), name='add-student'),
 
 ]
