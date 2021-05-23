efe227b8ddeb7442e051239d89371759c4437550
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-05-23 23:59:49 +0430

    Some Tests #1

--- account/tests/test_accounts_api.py
+++ account/tests/test_accounts_api.py
@@ -77,3 +77,11 @@ class PrivateAccountApiTests(APITestCase):
         response = self.client.get(LIST_ACCOUNTS_URL)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_creating_students_success(self):
+        """Test creating students is successful"""
+        pass
+
+    def test_listing_students_success(self):
+        """Test listing students is successful"""
+        pass
