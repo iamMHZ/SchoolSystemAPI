644601e17880583ef9e4fd937390ba047785fb06
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-05-24 13:52:34 +0430

    todo  #1

--- school/tests/test_news_api.py
+++ school/tests/test_news_api.py
@@ -101,3 +101,6 @@ class PrivateNewsApiTests(APITestCase):
         serializer = NewsDetailedSerializer(news)
 
         self.assertEqual(serializer.data, response.data)
+
+
+# TODO test listing news
