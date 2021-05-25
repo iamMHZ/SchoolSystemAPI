02d14a37ec5e60b17e202066fdf851d6095d4bab
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-05-25 13:11:55 +0430

    added authentication to user view #2

--- account/views.py
+++ account/views.py
@@ -2,19 +2,13 @@ from django.contrib.auth import get_user_model
 from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.permissions import IsAdminUser
 
-from account.serializers import StudentSerializer
+from account.serializers import StudentSerializer, UserSerializer
 
 
 class UserCreateView(CreateAPIView):
     """Create a new user base on the requested user"""
-
+    permission_classes = [IsAdminUser]
     serializer_class = UserSerializer
-
-    def get_serializer_class(self):
-        if self.request.user.is_superuser:
-            return self.serializer_class
-
-        return StudentSerializer
 
 
 class UserListView(ListAPIView):
