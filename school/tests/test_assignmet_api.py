ef76a3757f50622ec629800685f48abf438dc964
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-06-05 18:32:38 +0430

    Tests for the assignments api  #1

---
+++ school/tests/test_assignmet_api.py
@@ -0,0 +1,19 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+
+ASSIGNMENT_VIEWSET_URL = reverse('school:assignment-list')
+
+
+class PublicAssignmentApiTest(APITestCase):
+    """Tests for the assignments api that do not need authentication"""
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_assignment_api_needs_authentication(self):
+        """Test that assignments api needs authentication"""
+
+        response = self.client.get(ASSIGNMENT_VIEWSET_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
