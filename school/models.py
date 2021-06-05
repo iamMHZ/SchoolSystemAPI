58c7a7ffbe405f8a8c818005a859e3724ce1abb3
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-06-05 18:03:49 +0430

    commented submodels #3

--- school/models.py
+++ school/models.py
@@ -2,17 +2,31 @@ from django.conf import settings
 from django.db import models
 
 
-class News(models.Model):
+class AbstractBaseTask(models.Model):
+    """Abstract base task that has all the common fields of the new and assignment """
+
     teacher = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     body = models.TextField(max_length=600)
 
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, )
+
+    class Meta:
+        abstract = True
+
     def __str__(self):
         return str(self.title)
 
-# class Assignment(models.Model):
-#     pass
+
+class News(AbstractBaseTask):
+    """Model for the News"""
+    pass
 
 
-# class Task(models.Model):
-#     pass
+class Assignment(AbstractBaseTask):
+    """Model for the Assignments"""
+    pass
+    # TODO add pdf file to assignment
+    # TODO add answers to assignments
+    # file =
