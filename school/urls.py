6ac80e80b77c53f5ab3007916f70376d5e1edf76
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-06-05 18:21:59 +0430

    Added assignmets viewset to the router  #2

--- school/urls.py
+++ school/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from school.views import NewsViewSet, ListNewsView
+from school.views import NewsViewSet, ListNewsView, AssignmentsViewSet
 
 app_name = 'school'
 
 router = DefaultRouter()
 router.register('news', NewsViewSet)
+router.register('assignments', AssignmentsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
